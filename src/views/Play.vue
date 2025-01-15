0f975c98dcee725d50e8d09b65435eb75d0b4166
Amir JCS <amirmahdyjdx@gmail.com>
2025-01-15 22:14:48 +0330

    feat: complete decode options in play view

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { reactive } from 'vue';
+
+class Options {
+    cols: number = 0;
+    rows: number = 0;
+    bombs: number = 0
+}
+
+const route = useRoute()
+const options: Options = reactive<Options>(JSON.parse(atob(route.params.playcode as string)))
+console.log(options);
+
+</script>
 <template>
-    Play
+    Play in {{ options.cols }} × {{ options.rows }} board with {{ options.bombs }} bombs
 </template>
